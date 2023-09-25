2774fed98abde61c74f93d3201eb5695ddedea3a
Captainbrooks <gairemilton209@gmail.com>
2023-09-24 20:16:25 -0400

    Initial

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.getElementById('nav');
-    const closeIcon = document.querySelector('.menu-toggle .close-icon');
-
-    menuToggle.classList.toggle('active');
-    nav.classList.toggle('active');
-    closeIcon.classList.toggle('active'); // Toggle the "X" icon
-}
